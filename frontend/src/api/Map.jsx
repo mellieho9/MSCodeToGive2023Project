6cd5a0b08a82fc5b0fabbd84664305075a469c6b
ambitiousjulian <julian@Julians-MBP.attlocal.net>
2023-04-24 07:16:25 -0400

    pull backend coordinates, fix cors policy on Routes.db

--- frontend/src/api/Map.jsx
+++ frontend/src/api/Map.jsx
@@ -1,111 +1,91 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Icon, Input, Stack } from "@chakra-ui/react";
-import { FaMapMarkerAlt, FaTrashAlt } from "react-icons/fa";
-
+import React, { useState, useEffect, useRef } from "react";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import XYZ from "ol/source/XYZ";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
+import { Vector } from "ol/source";
+import { Vector as VectorLayer } from "ol/layer";
+import { Style, Icon } from "ol/style";
 import Feature from "ol/Feature";
 import Point from "ol/geom/Point";
 import LineString from "ol/geom/LineString";
-import { fromLonLat } from "ol/proj";
-import { Icon as iconOL, Style, Stroke } from "ol/style";
+import { defaults as defaultControls, FullScreen } from 'ol/control';
 
-import "../css/Map.css";
-
-function MapComponent() {
-  const [map, setMap] = useState();
+const MapComponent = () => {
   const [marker, setMarker] = useState([]);
-
-  const mapRef = useRef(null);
-
   useEffect(() => {
-    const initialMap = new Map({
-      target: "map-container",
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-          }),
+    const fetchMarkers = async () => {
+      const response = await fetch("http://127.0.0.1:5000/summary");
+      const data = await response.json();
+      
+      const parsedData = data.map((item) => JSON.parse(item[0]));
+
+      const markers = parsedData.map((item) => {
+        if (!item.partner) return null;
+        const { latitude, longitude, name } = item.partner;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}, Name: ${name}`);
+        return new Feature({
+          geometry: new Point(fromLonLat([longitude, latitude])),
+          name,
+        });
+      }).filter((item) => item !== null); 
+
+      const lineCoords = parsedData.map((item) => {
+        const { latitude, longitude } = item.partner;
+        return fromLonLat([longitude, latitude]);
+      });
+
+      const line = new Feature({
+        geometry: new LineString(lineCoords),
+      });
+
+      setMarker([...markers, line]);
+
+      const lineLayer = new VectorLayer({
+        source: new Vector({
+          features: [line],
         }),
-
-        new VectorLayer({
-          source: new VectorSource({
-            features: [
-              new Feature({
-                geometry: new Point(fromLonLat([ -83.57, 34.47])),
-                name: "Marker 1",
-              }),
-              new Feature({
-                geometry: new Point(fromLonLat([-84.42, 33.76 ])),
-                name: "Marker 2",
-              }),
-              new Feature({
-                geometry: new Point(fromLonLat([-83.83, 34.01 ])),
-                name: "Marker 3",
-              }),
-              new Feature({
-                geometry: new Point(fromLonLat([-84.67,34.06 	])),
-                name: "Marker 4",
-              }),
-              new Feature({
-                geometry: new Point(fromLonLat([ -82.57, 32.19 ])),
-                name: "Marker 5",
-              }),
-            ],
-          }),
-          style: new Style({
-            image: new iconOL({
-              anchor: [0.5, 46],
-              anchorXUnits: "fraction",
-              anchorYUnits: "pixels",
-              src: "https://openlayers.org/en/latest/examples/data/icon.png",
-            }),
-          }),
-        }),
-
-        new VectorLayer({
-          source: new VectorSource({
-            features: [
-              new Feature({
-                geometry: new LineString([
-                  fromLonLat([	-83.57, 34.47]),
-                  fromLonLat([	-84.42, 33.76 ]),
-                  fromLonLat([-83.83, 34.01]),
-                  fromLonLat([	-84.67,34.06 ]),
-                  fromLonLat([-82.57, 32.19]),
-                ]),
-              }),
-            ],
-          }),
-          style: new Style({
-            stroke: new Stroke({
-              color: "red",
-              width: 2,
-            }),
-          }),
-        }),
-      ],
-
-      view: new View({
-        center: fromLonLat([-98.5795, 39.8283]),
-        zoom: 4,
-      }),
-    });
-
-    setMap(initialMap);
-
-    return () => {
-      initialMap.dispose();
+        
+      });
     };
+
+    fetchMarkers();
+
   }, []);
+  
 
-  return <div id="map-container" className="map" style={{ height: '600px', width:'100%', margin: "0 auto",}} />;
+  const map = new Map({
+    target: "map-container",
+    layers: [
+      new TileLayer({
+        source: new XYZ({
+          url:
+            "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        }),
+      }),
+      new VectorLayer({
+        source: new Vector({
+          features: marker,
+        }),
+        style: new Style({
+          image: new Icon({
+            src: "https://openlayers.org/en/latest/examples/data/icon.png",
+            scale: 0.90,
+          }),
+        }),
+      }),
+    ],
+    view: new View({
+      center: fromLonLat([-84.936, 34.485]),
+      zoom: 12,
+    }),
+    controls: defaultControls().extend([
+      new FullScreen(),
+    ]),
+  });
+
+  return <div id="map-container" style={{ width: "100%", height: "400px" }}></div>;
 };
-
 
 export default MapComponent;
